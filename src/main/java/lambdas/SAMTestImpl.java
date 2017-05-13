c25a0f6f4fbc6899d728c822b3055e69f8ee3da7
Zenifer Cheruveettil <zen_cv@yahoo.com>
2017-05-13 08:48:18 +0200

    SAM test client

---
+++ src/main/java/lambdas/SAMTestImpl.java
@@ -0,0 +1,9 @@
+package lambdas;
+
+//Implementation of a Single Abstract Method Interface with a default method
+public class SAMTestImpl implements SAMTest {
+    @Override
+    public void blah() {
+        System.out.println("Blah implemented");
+    }
+}
